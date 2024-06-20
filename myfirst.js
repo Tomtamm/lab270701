6acd075d1dfa80c3f8afa822f13191674cb3bdff
Tomtamm <apichaya.sm2001@gmail.com>
2024-06-20 02:02:53 +0000

    first

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,6 @@
 let http = require('http');
 
-http.createServer(funtion (req, res) {
+http.createServer(function (req, res) {
     res.writeHead(200, {'content-Type': 'Text/html'});
     res.end('Hello World');
 }).listen(9000);
